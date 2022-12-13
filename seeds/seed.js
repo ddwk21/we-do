7a208d7931c469c18d71548d4a0c7ad0e8f5f777
Mitch Codes <ddwk21@gmail.com>
2022-12-12 20:54:34 -0600

    usergen added

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,44 @@
 const sequelize = require('../config/connection');
 const { User, Message, Event } = require('../models');
 
+const{randFirstName, randEmail, randPassword, randZipCode} = require("@ngneat/falso")
+
+const activities = ['golf', 'hiking', 'swimming', 'basketball', 'gaming']
+
 const userData = require('./userData.json');
 const messageData = require('./messageData.json')
 const eventData = require('./eventData.json')
+
+function userGen(){
+  let gennedUsers = []
+
+  for(let i = 0; i<100; i++){
+    let user = {
+      username: randFirstName(),
+      email: randEmail(),
+      password: randPassword(),
+      zip_code: randZipCode(),
+      interest: activities[Math.floor(Math.random()*activities.length)]
+
+    }
+    gennedUsers.push(user)
+    
+  }
+  return gennedUsers;
+}
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  let additionalUsers = userGen()
+  console.log(additionalUsers)
+
+  await User.bulkCreate([...
+    userData,
+    ...additionalUsers],
+     
+    {
     individualHooks: true,
     returning: true,
   });
@@ -21,7 +50,6 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
 
   process.exit(0);
 };
