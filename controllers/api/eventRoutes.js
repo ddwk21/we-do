36493471eb4a99a06ef079ca8c0da2fc0256d274
Jesus M <marquez.jay444@gmail.com>
2022-12-15 17:47:32 -0600

    updated geocode api

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const router = require('express').Router();
 const { Event } = require('../../models');
 const { findByPk } = require('../../models/User');
@@ -22,17 +23,20 @@ router.post( '/', async(req, res) => {
 
 router.put('/join/:eventId',async(req,res)=>{
     //Query Event by event ID and pull participants array
-    
+    console.log(req.body)
    try{ 
     const event = await Event.findByPk(req.params.eventId)
-    
+
+    const geocodeResponse = await axios(req.body.geocodeUrl)    
+
+    console.log(JSON.stringify(geocodeResponse.data,null, 2))
     if(event.number_of_participants<event.max_participants)
         {event.participants.push(req.session.user_id);
         event.number_of_participants+=1;
 
         await event.save()
         console.log('Success!')
-        res.status(200)
+        res.status(200).json(geocodeResponse.data)
     }else{
         res.status(409).json({message:'No tank you'})
     }
