c9ad3003bd6b2e2953d9aaa8067c6f3f592ce69b
Jesus M <marquez.jay444@gmail.com>
2022-12-15 16:58:34 -0600

    created a fetch respone in events.js

--- public/js/events.js
+++ public/js/events.js
@@ -1,10 +1,35 @@
 function joinEventHandler(e){
-e.preventDefault();
-const eventLocation = e.target.dataset.location
-    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent('Milwaukee, Wisconsin')}&destinations=${encodeURIComponent (eventLocation)}&units=imperial&key=AIzaSyDDMvRzhH0p3eGfFPLj2wPrPf3Tv2IhiVE`
-    console.log(url)
+  e.preventDefault();
+  const eventLocation = e.target.dataset.location
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent('53215')}&destinations=${encodeURIComponent(eventLocation)}&units=imperial&key=AIzaSyDDMvRzhH0p3eGfFPLj2wPrPf3Tv2IhiVE`
+    let geocodedEvents = {
+      name: nameInput.value,
+      max_participants: maxPartInput.value,
+      description: descInput.value,
+      location: locInput.value,
+      time_and_date: timeInput.value,
+      user_id:[],
+      tags: tagInput.value
+    }
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://maps.googleapis.com/maps/api/distancematrix/json?',
+        'mode': 'no-cors'
+      },
+      body: JSON.stringify(geocodedEvents)
+    })
+    .then((res)=>{
+      location.href = '/'
+    })
+    .catch((err)=>alert('location not found'))
+
+
+    
   }
-// document.getElementsByClassName('joinBtn').
+
+
 
 
 Array.from (document.getElementsByClassName('joinBtn')).forEach(btn=> btn.addEventListener('click', joinEventHandler))
--- utils/helpers.js
+++ utils/helpers.js
@@ -17,7 +17,7 @@ module.exports = {
   get_distance: () => {
     var config = {
       method: 'get',
-      url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=AIzaSyDDMvRzhH0p3eGfFPLj2wPrPf3Tv2IhiVE',
+      url: 'https://maps.googleapis.com/maps/api/distancematrix/json?',
       headers: { }
     };
     
